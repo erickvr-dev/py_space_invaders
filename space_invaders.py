fc6adf4ff0f7ffbbb77963b52ed12b8a23fb26e6
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-04 18:18:56 -0400

    setting the bg color

--- space_invaders.py
+++ space_invaders.py
@@ -11,6 +11,7 @@ class SpaceInvaders:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Space Invaders!")
         self.clock = pygame.time.Clock() # creating a clock to manage the correct frame rate
+        self.bg_color = (230, 230, 230) # Set the background color to light gray
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -19,9 +20,16 @@ class SpaceInvaders:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Fill the screen with background color
+            self.screen.fill(self.bg_color) 
+
             # Make the most recently drawn screen visible.
             pygame.display.flip()
-            self.clock.tick(60) # Stablish the clock to maintain 60 times per second
+
+            # Stablish the clock to maintain 60 times per second
+            self.clock.tick(60) 
+            
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
