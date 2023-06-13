91c70e435d708fc0c1b73a319b75323189a11124
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-13 11:27:15 -0400

    drawing the ship to the screen

--- space_invaders.py
+++ space_invaders.py
@@ -4,6 +4,8 @@ import pygame
 
 from settings import Settings
 
+from ship import Ship
+
 class SpaceInvaders:
     """Overall class to manage game assets and behavior."""
     def __init__(self):
@@ -17,6 +19,8 @@ class SpaceInvaders:
         self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         
         pygame.display.set_caption("Space Invaders! by Sp0ck")
+
+        self.ship = Ship(self) # creates a ship object giving self reference to the game.
         
     def run_game(self):
         """Start the main loop for the game"""
@@ -26,8 +30,9 @@ class SpaceInvaders:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
-            # Fill the screen with background color
+            # Redraw the screen during each pass through the loop
             self.screen.fill(self.settings.bg_color) 
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
