473a009ba58623d762a04c8d5eb858151e932699
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-13 13:40:52 -0400

    responding to a keypress RIGHT key

--- space_invaders.py
+++ space_invaders.py
@@ -25,20 +25,32 @@ class SpaceInvaders:
     def run_game(self):
         """Start the main loop for the game"""
         while True:
-            # Watch for keyboard and mouse events.
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-            
-            # Redraw the screen during each pass through the loop
-            self.screen.fill(self.settings.bg_color) 
-            self.ship.blitme()
+            self._check_events()
 
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
 
             # Stablish the clock to maintain 60 times per second
-            self.clock.tick(60) 
+            self.clock.tick(60)
+    
+    def _check_events(self):
+        """Respond to keypresses and mouse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen"""
+        
+        # Redraw the screen during each pass through the loop
+        self.screen.fill(self.settings.bg_color) 
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
             
 
 if __name__ == '__main__':
