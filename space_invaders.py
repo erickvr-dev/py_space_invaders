dacd075826ba0a0ef007e7a4acf3df14403101e6
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-13 14:34:46 -0400

    allow continuous movement Right Key

--- space_invaders.py
+++ space_invaders.py
@@ -26,7 +26,7 @@ class SpaceInvaders:
         """Start the main loop for the game"""
         while True:
             self._check_events()
-
+            self.ship.update()
             self._update_screen()
 
             # Stablish the clock to maintain 60 times per second
@@ -39,8 +39,10 @@ class SpaceInvaders:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
