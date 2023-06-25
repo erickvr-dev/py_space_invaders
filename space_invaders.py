bd8ff1963f6afd5f63c9d35201cd596b461af9aa
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-25 18:18:58 -0400

    starting the game when Play is pressed

--- space_invaders.py
+++ space_invaders.py
@@ -8,6 +8,8 @@ from settings import Settings
 
 from game_stats import GameStats
 
+from button import Button
+
 from ship import Ship
 
 from bullet import Bullet
@@ -48,6 +50,9 @@ class SpaceInvaders:
 
         # Start Space Invaders in an inactive state.
         self.game_active = False
+
+        # Make the Play button.
+        self.play_button = Button(self, "Play!")
         
     def run_game(self):
         """Start the main loop for the game"""
@@ -74,6 +79,15 @@ class SpaceInvaders:
 
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mouse_pos = pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.game_active = True
 
     
     def _check_keydown_events(self, event):
@@ -212,6 +226,10 @@ class SpaceInvaders:
             bullet.draw_bullet()
         self.ship.blitme()
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive
+        if not self.game_active:
+            self.play_button.draw_button()
 
         # Make the most recently drawn screen visible.
         pygame.display.flip()
