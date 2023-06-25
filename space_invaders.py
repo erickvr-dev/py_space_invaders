75dac0f3849cd5da5fcca23ef1b9ce9ab8c9c661
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-25 17:04:20 -0400

    Alien that reach the bottom of the screen

--- space_invaders.py
+++ space_invaders.py
@@ -126,6 +126,9 @@ class SpaceInvaders:
         if pygame.sprite.spritecollideany(self.ship, self.aliens):
             self._ship_hit()
 
+        # Look for aliens hitting the bottom of the screen.
+        self._check_aliens_bottom()
+
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
@@ -181,6 +184,14 @@ class SpaceInvaders:
         for alien in self.aliens.sprites():
             alien.rect.y += self.settings.fleet_drop_speed
         self.settings.fleet_direction *= -1
+
+    def _check_aliens_bottom(self):
+        """Check if any aliens have reached the bottom of the screen."""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # Treat this the same as if the ship got hit.
+                self._ship_hit()
+                break
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
