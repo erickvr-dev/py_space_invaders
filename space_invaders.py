99e9369e0959f90297e9d33a26a397d511b4c433
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-25 11:10:12 -0400

    dropping the fleet and changing direction

--- space_invaders.py
+++ space_invaders.py
@@ -98,7 +98,8 @@ class SpaceInvaders:
         # print(len(self.bullets)) this line is just to make sure that old bullets are deleted.
 
     def _update_aliens(self):
-        """Update the positions of all aliens in the fleet."""
+        """Check if the fleet is at an edge, then update positions."""
+        self._check_fleet_edges()
         self.aliens.update()
 
 
@@ -127,6 +128,19 @@ class SpaceInvaders:
         new_alien.rect.x = x_position
         new_alien.rect.y = y_position
         self.aliens.add(new_alien)
+
+    def _check_fleet_edges(self):
+        """Respond appropriately if any aliens have reached an edge."""
+        for alien in self.aliens.sprites():
+            if alien.check_edges():
+                self._change_fleet_direction()
+                break
+
+    def _change_fleet_direction(self):
+        """Drop the entire fleet and change the fleet's direction."""
+        for alien in self.aliens.sprites():
+            alien.rect.y += self.settings.fleet_drop_speed
+        self.settings.fleet_direction *= -1
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
