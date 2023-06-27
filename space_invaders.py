c5280d575bc7b7df0a817a35ac8bfdee73c37925
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-27 12:04:36 -0400

    high score implementation

--- space_invaders.py
+++ space_invaders.py
@@ -94,6 +94,7 @@ class SpaceInvaders:
             # Reset the game settings.
             self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
+            self.sb.prep_score()
             self.game_active = True
 
             # Get rid of any remaining bullets and aliens.
@@ -149,6 +150,12 @@ class SpaceInvaders:
         """Respond to bullet-alien collisions."""
         # Remove any bullets and aliens that have collided.
         collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True) # change to False and True to make inmortal bullets.
+
+        if collisions:
+            for aliens in collisions.values():
+                self.stats.score += self.settings.alien_points * len(aliens)
+            self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
