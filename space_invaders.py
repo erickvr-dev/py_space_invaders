6d35ab8b98b5a90deb5b1b48b60dfcc883260c0e
Erick_VR <98334211+erickvr2k@users.noreply.github.com>
2023-06-27 10:26:00 -0400

    leveling up

--- space_invaders.py
+++ space_invaders.py
@@ -88,7 +88,8 @@ class SpaceInvaders:
         """Start a new game when the player clicks Play."""
         button_clicked = self.play_button.rect.collidepoint(mouse_pos)
         if button_clicked and not self.game_active:
-            # Reset the game statistics.
+            # Reset the game settings.
+            self.settings.initialize_dynamic_settings()
             self.stats.reset_stats()
             self.game_active = True
 
@@ -150,6 +151,7 @@ class SpaceInvaders:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
+            self.settings.increase_speed()
 
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update positions."""
